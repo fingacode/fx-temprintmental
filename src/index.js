449785abcb6adeef89772c7335a7bc51311ad01b
Junior Ngoma <junior@kidindustry.co.uk>
2021-11-18 04:30:03 +0000

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // these are the variables you can use as inputs to your algorithms
-console.log(fxhash)   // the 64 chars hex number fed to your algorithm
+// console.log(fxhash)   // the 64 chars hex number fed to your algorithm
 // console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
 
 // note about the fxrand() function 
